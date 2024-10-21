604f5fc480edb808bc459e776ac7070f84c260b2
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-10-21 21:58:45 +0300

    refactor: create pure functions

--- js/app.js
+++ js/app.js
@@ -9,20 +9,26 @@ window.addEventListener("scroll", activeState);
 for (let i = 0; i < allSections.length; i++) {
   const navItem = document.createElement("li");
   const navLink = document.createElement("a");
-  navLink.classList.add("menu__link");
+  navLink.classList.add("nav__link");
   navLink.innerText = "Section " + (i + 1);
   navLink.href = "#section" + (i + 1);
 
+  smoothScroll(navLink);
+
+  navItem.appendChild(navLink);
+  navList.appendChild(navItem);
+}
+
+// Scroll to Section -pure Function
+function smoothScroll(navLink) {
   navLink.addEventListener("click", (event) => {
     event.preventDefault();
+
     document.querySelector(navLink.getAttribute("href")).scrollIntoView({
       behavior: "smooth",
       block: "start",
     });
   });
-
-  navItem.appendChild(navLink);
-  navList.appendChild(navItem);
 }
 
 // crete pure function for checking if the section has intersected or yet
@@ -36,10 +42,8 @@ function activeState() {
   allSections.forEach((sec) => {
     const correspondingLink = navList.querySelector(`a[href="#${sec.id}"]`);
     if (hasIntersect(sec)) {
-      sec.classList.add("active");
       correspondingLink.classList.add("active");
     } else {
-      sec.classList.remove("active");
       correspondingLink.classList.remove("active");
     }
   });
