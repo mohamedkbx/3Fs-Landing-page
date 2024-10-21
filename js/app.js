177cb027915616740227caf9826f4e6bba51fab6
mohamedkbx <109092116+mohamedkbx@users.noreply.github.com>
2024-10-21 19:13:27 +0300

    refactor : reformat js file

--- js/app.js
+++ js/app.js
@@ -1,39 +1,3 @@
-/**
- *
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- *
- * Dependencies: None
- *
- * JS Version: ES2015/ES6
- *
- * JS Standard: ESlint
- *
- */
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
- */
-
-/**
- * Define Global Variables
- *
- */
-
-/**
- * End Global Variables
- * Start Helper Functions
- *
- */
-
-/**
- * End Helper Functions
- * Begin Main Functions
- *
- */
 // Select the navbar and sections
 const navList = document.getElementById("navbar__list");
 const sections = document.querySelectorAll("section");
@@ -86,17 +50,3 @@ window.addEventListener("scroll", setActiveSection);
 
 // Run once on load to set the active section
 setActiveSection();
-
-// Scroll to anchor ID using scrollTO event
-
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
